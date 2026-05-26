9ff2b2de9d9b3d26dbecf9600f3cb2eada39bb59
Neeraj <ndagar7844@gmail.com>
2026-05-26 11:20:59 +0530

    Fixed axios API config

--- frontend/admin/src/api.js
+++ frontend/admin/src/api.js
@@ -1,3 +1,10 @@
-const API = "https://cms-3tty.onrender.com";
+import axios from "axios";
+
+const API = axios.create({
+
+  baseURL:
+    "https://cms-3tty.onrender.com/api",
+
+});
 
 export default API;
