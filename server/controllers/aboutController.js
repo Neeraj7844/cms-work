5fac38c1c100fb1c36680db5f332104685035c59
Neeraj <ndagar7844@gmail.com>
2026-06-16 16:31:42 +0530

    Fix upload image URL for Render

--- server/controllers/aboutController.js
+++ server/controllers/aboutController.js
@@ -52,7 +52,7 @@ req.files[name][0]
 
 ){
 
-return `http://localhost:5000/uploads/${req.files[name][0].filename}`;
+return `https://cms-work.onrender.com/uploads/${req.files[name][0].filename}`;
 
 }
 
